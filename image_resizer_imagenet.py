1544e19e5dd1a27670c57f009e1cedaf151239f1
Shichang Tang <tangshch@shanghaitech.edu.cn>
2018-12-19 22:01:51 +0800

    run 8 threads in parallel, use 'box' by default

--- image_resizer_imagenet.py
+++ image_resizer_imagenet.py
@@ -18,13 +18,13 @@ def parse_arguments():
     parser.add_argument('-i', '--in_dir', help="Input directory with source images", required=True)
     parser.add_argument('-o', '--out_dir', help="Output directory for resized images", required=True)
     parser.add_argument('-s', '--size', help="Size of an output image (e.g. 32 results in (32x32) image)",
-                        default=32, type=int)
+                        default=128, type=int)
     parser.add_argument('-a', '--algorithm', help="Algorithm used for resampling: lanczos, nearest,"
                                                   " bilinear, bicubic, box, hamming",
-                        default='lanczos')
+                        default='box')
 
     parser.add_argument('-r', '--recurrent', help="Process all subfolders in this folder (1 lvl deep)",
-                        action='store_true')
+                        action='store_true', default=1)
     parser.add_argument('-f', '--full', help="Use all algorithms, create subdirectory for each algorithm output",
                         action='store_true')
     parser.add_argument('-e', '--every_nth', help="Use if you don't want to take all classes, "
@@ -32,7 +32,7 @@ def parse_arguments():
                         default=1, type=int)
     parser.add_argument('-j', '--processes', help="Number of sub-processes that run different folders "
                                                   "in the same time ",
-                        default=1, type=int)
+                        default=8, type=int)
     args = parser.parse_args()
 
     return args.in_dir, args.out_dir, args.algorithm, args.size, args.recurrent, \
